43bbe3e1fe58d37656e1d238cdb4eb8601bb5f80
Divya Jain <dkj@somaiya.edu>
2020-06-12 21:17:06 +0530

    add day 3 solution (2019)

---
+++ 2019/Day03/part1.py
@@ -0,0 +1,44 @@
+from collections import defaultdict, namedtuple
+
+
+Cell = namedtuple("Cell", ["x", "y"])
+
+move_map = {"R": Cell(1, 0), "L": Cell(-1, 0), "U": Cell(0, 1), "D": Cell(0, -1)}
+
+
+def move(cell, move_key):
+    move_delta = move_map[move_key]
+    return Cell(cell.x + move_delta.x, cell.y + move_delta.y)
+
+
+def get_visited_cells(start_point, move_code):
+    visited_cells = [start_point]
+    for _ in range(int(move_code[1:])):
+        visited_cells.append(move(visited_cells[-1], move_code[0]))
+    return visited_cells[1:]
+
+
+def get_intersections(wires):
+    wires_visited = defaultdict(lambda: [False] * len(wires))
+
+    for i, wire in enumerate(wires):
+        current_position = Cell(0, 0)
+        for move_code in wire:
+            visited_cells = get_visited_cells(current_position, move_code)
+            current_position = visited_cells[-1]
+            for visited_cell in visited_cells:
+                wires_visited[visited_cell][i] = True
+
+    return [cell for cell, visited in wires_visited.items() if all(visited)]
+
+
+def get_distance(cell1, cell2=Cell(0, 0)):
+    return abs(cell1.x - cell2.x) + abs(cell1.y + cell2.y)
+
+
+if __name__ == "__main__":
+    with open("input", "r") as input_file:
+        wires = [wire.split(",") for wire in input_file]
+        intersections = get_intersections(wires)
+        closest_intersection = min(intersections, key=get_distance)
+        print(get_distance(closest_intersection))
---
+++ 2019/Day03/part2.py
@@ -0,0 +1,39 @@
+from part1 import Cell, get_visited_cells, get_intersections
+
+
+def get_min_steps(wires):
+    intersections = get_intersections(wires)
+
+    steps_to_intersection_map = [[0 for _ in wires] for _ in intersections]
+
+    for j, wire in enumerate(wires):
+        wire_path = [Cell(0, 0)]
+        visited_cells = set(wire_path)
+
+        for move in wire:
+            new_visited_cells = get_visited_cells(wire_path[-1], move)
+            previously_visited_cell = next(
+                (cell in visited_cells for cell in new_visited_cells), None
+            )
+            if previously_visited_cell:
+                wire_path = wire_path[: wire_path.index(previously_visited_cell)]
+                wire_path.extend(
+                    new_visited_cells[
+                        new_visited_cells.index(previously_visited_cell) :
+                    ]
+                )
+            else:
+                wire_path.extend(new_visited_cells)
+
+            visited_cells.update(new_visited_cells)
+
+            for i, intersection in enumerate(intersections):
+                if intersection in new_visited_cells:
+                    steps_to_intersection_map[i][j] = wire_path.index(intersection)
+
+    return min(sum(steps) for steps in steps_to_intersection_map)
+
+
+with open("input", "r") as input_file:
+    wires = [wire.split(",") for wire in input_file]
+    print(get_min_steps(wires))
